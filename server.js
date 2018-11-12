a6f23c175bec5abdd69c5658841c304e6c1b2296
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-12 11:24:28 -0800

    added static server

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 'use strict';
 
 // Load array of notes
+const express = require('express');
+
 const data = require('./db/notes');
+
+const app = express();
 
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+
+app.use(express.static('public'));
+
+app.listen(8080, function(){
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
