de4b4ce4c666d47a02a6e5b5f623002b9b5ff21b
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-12 12:22:56 -0800

    search is working and accounts for empty search bar

--- server.js
+++ server.js
@@ -13,9 +13,19 @@ console.log('Hello Noteful!');
 
 app.use(express.static('public'));
 
-app.get('/api/notes/:id', (req, res) => {
-  res.json(data.find(item => item.id === parseInt(req.params.id)));
+// app.get('/api/notes/:id', (req, res) => {
+//   res.json(data.find(item => item.id === parseInt(req.params.id)));
+// });
+
+app.get('/api/notes/', (req, res) => {
+  // console.log(req.query);
+  if (req.query.searchTerm){
+    res.json(data.filter(item => item.title.includes(req.query.searchTerm) || item.content.includes(req.query.searchTerm)));
+  } else {
+    res.json(data);
+  }
 });
+
 
 app.listen(8080, function(){
   console.info(`Server listening on ${this.address().port}`);
