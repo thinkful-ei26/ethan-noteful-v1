03a3330795582901a003298c5b08db8dc3ea936c
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-12 11:37:55 -0800

    calling single GET endpoint

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ console.log('Hello Noteful!');
 
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
 app.listen(8080, function(){
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
