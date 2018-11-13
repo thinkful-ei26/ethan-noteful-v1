b6661146d1bd90c95a182f0f10efbf3bd224233c
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-13 11:57:34 -0800

    more error handling

--- server.js
+++ server.js
@@ -33,13 +33,23 @@ app.get('/api/notes/', (req, res) => {
   }
 });
 
+app.get('/boom', (req, res, next) =>{
+  throw new Error ('Boom!');
+});
+
 app.use(function (req, res, next){
   let err = new Error('Not Found');
   err.status = 404;
   res.status(404).json({message: 'Not Found'});
 });
 
-app
+app.use(function (err, req, res, next){
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
 
 app.listen(PORT, function(){
   console.info(`Server listening on ${this.address().port}`);
