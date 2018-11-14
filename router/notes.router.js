b239c93b954f6d5bf9c12f590a8ef7e6f751ccc5
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-14 12:30:43 -0800

    post new notes is working

--- router/notes.router.js
+++ router/notes.router.js
@@ -50,7 +50,25 @@ router.put('/notes/:id', (req, res, next) => {
 });
 
 
-router.post('/notes/:id')
+router.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+  if (!newItem.title) {
+    const err = new Error('Missing title in request body');
+    err.status = 400;
+    return next(err);
+  }
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    } if (item) {
+      res.location(`http://${req.headers.host}/notes/${item}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
 
 router.delete('/notes/:id', (req, res, next) => {
   const id = req.params.id;
