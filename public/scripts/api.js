8db0673734817594079f692dd1cae4bc8f19196a
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-15 11:54:32 -0800

    promisified search

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -3,15 +3,24 @@
 
 const api = {
 
-  search: function (query, callback) {
-    $.ajax({
+  // search: function (query, callback) {
+  //   $.ajax({
+  //     type: 'GET',
+  //     url: '/api/notes/',
+  //     dataType: 'json',
+  //     data: query,
+  //     success: callback
+  //   });
+  // },
+  search: function (query) {
+    return $.ajax({
       type: 'GET',
-      url: '/api/notes/',
+      url: '/api/notes',
       dataType: 'json',
-      data: query,
-      success: callback
+      data: query
     });
   },
+
 
   details: function (id, callback) {
     $.ajax({
